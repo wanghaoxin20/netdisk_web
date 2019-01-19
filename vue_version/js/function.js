becc2ffdb5a57a96f8a37807cb1217c49df9c80d
wanghaoxin <wang.haoxin@nexuslink.cn>
2019-01-19 23:52:00 +0800

    添加删除文件和上传文件的初始功能ui

--- vue_version/js/function.js
+++ vue_version/js/function.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
   var navHeader = new Vue({
     el: "#nav-header",
     data: {
@@ -18,7 +18,7 @@ window.onload = function() {
       active: 0
     },
     methods: {
-      changeItem: function(val) {
+      changeItem: function (val) {
         var items = document.getElementsByClassName("bar-item");
         items[this.active].classList.remove("itemActive");
         items[val].classList.add("itemActive");
@@ -44,7 +44,7 @@ window.onload = function() {
     },
     methods: {
       //请求并更新表格数据
-      update: function(path) {
+      update: function (path) {
         var that = this;
         axios
           .post(
@@ -52,7 +52,7 @@ window.onload = function() {
             { path: path },
             { headers: { "Content-Type": "application/json;charset=utf-8" } }
           )
-          .then(function(res) {
+          .then(function (res) {
             //成功请求后返回数据的处理
             if (res.data.status == 200) {
               that.tableData = res.data.data;
@@ -61,19 +61,19 @@ window.onload = function() {
               alert("error:" + res.data.msg);
             }
           })
-          .catch(function(error) {
+          .catch(function (error) {
             alert("error" + error);
           });
       },
       //双击表格行
-      trDBClick: function(index) {
+      trDBClick: function (index) {
         var fp = this.tableData[index];
         if (fp.isFile == false) {
           this.update(this.crtPath + fp.name + "/");
         }
       },
       //下载文件
-      download: function(index) {
+      download: function (index) {
         var fp = this.tableData[index];
         if (fp.isFile == true) {
           window.open(
@@ -81,8 +81,31 @@ window.onload = function() {
           );
         }
       },
+
+      //删除文件或者文件夹
+      del: function (index) {
+        var f = this.tableData[index];
+        var that = this;
+        axios
+          .post("http://localhost:8080/netdisk/deldir", {
+            path: that.crtPath,
+            name: f.name
+          })
+          .then(function (res) {
+            //成功请求后返回数据的处理
+            if (res.data.status == 200) {
+              that.update(that.crtPath);
+            } else if (res.data.status == 400) {
+              alert("error:" + res.data.msg);
+            }
+          })
+          .catch(function (error) {
+            alert("error" + error);
+          });
+      },
+
       //获取路径
-      getPaths: function() {
+      getPaths: function () {
         var strs = this.crtPath.split("/");
         var reg = /^\/+|\s+$/;
         var paths = [];
@@ -96,7 +119,7 @@ window.onload = function() {
         return paths;
       },
       //跳转目录
-      cdTo: function(index) {
+      cdTo: function (index) {
         var paths = this.getPaths();
         var p = "";
         //拼接路径path
@@ -106,8 +129,11 @@ window.onload = function() {
         }
         this.update(p + "/");
       },
-      showDNPane: function() {
+      showMkdirPane: function () {
         darkpane1.$data.dkp1Style.display = "";
+      },
+      showUploadPane: function () {
+        darkpane2.$data.dkp2Style.display = "";
       }
     },
     //初始化
@@ -123,14 +149,14 @@ window.onload = function() {
         height: window.innerHeight + "px",
         display: "none"
       },
-      adpStyle: {
+      windowStyle: {
         left: (window.innerWidth - 350) / 2 + "px",
         top: (window.innerHeight - 240) / 2 + "px"
       },
       dirname: ""
     },
     methods: {
-      mkdir: function() {
+      mkdir: function () {
         var that = this;
         var reg = /^\s+$/;
         if (reg.test(this.dirname) || this.dirname == "") {
@@ -141,7 +167,7 @@ window.onload = function() {
               path: pane.$data.crtPath,
               dirname: that.dirname
             })
-            .then(function(res) {
+            .then(function (res) {
               //成功请求后返回数据的处理
               if (res.data.status == 200) {
                 pane.update(pane.$data.crtPath);
@@ -150,27 +176,80 @@ window.onload = function() {
                 alert("error:" + res.data.msg);
               }
             })
-            .catch(function(error) {
+            .catch(function (error) {
               alert("error" + error);
             });
         }
       },
-      close: function() {
+      close: function () {
         this.dirname = "";
         this.dkp1Style.display = "none";
       }
     }
   });
 
+
+  var darkpane2 = new Vue({
+    el: "#darkpane2",
+    data: {
+      dkp2Style: {
+        height: window.innerHeight + "px",
+        display: "none"
+      },
+      windowStyle: {
+        left: (window.innerWidth - 350) / 2 + "px",
+        top: (window.innerHeight - 240) / 2 + "px"
+      }
+    },
+    methods: {
+      uploadFile: function () {
+        var that = this;
+        var file = document.getElementById("file").files[0];
+        if (file == null) {
+          alert("选择文件为空");
+        } else {
+          var form = new FormData();
+          form.append("file", file);
+          form.append("path", pane.$data.crtPath);
+          axios.post("http://localhost:8080/netdisk/uploadfile", form, { headers: { "Content-Type": "multipart/form-data" } }).
+            then(function (res) {
+              if (res.data.status == 200) {
+                pane.update(pane.$data.crtPath);
+                alert("成功上传[" + file.name + "]");
+              } else if (res.data.status == 400) {
+                alert("上传失败");
+              }
+              that.close();
+            }).catch(function (error) {
+              alert(error);
+            })
+        }
+      },
+      inputClick: function () {
+        document.getElementById("file").click();
+      },
+      close: function () {
+        document.getElementById("file").files[0] = null;
+        this.dkp2Style.display = "none";
+      }
+    }
+  });
+
+
+
   //窗口resize事件
-  window.onresize = function() {
+  window.onresize = function () {
     leftBar.$data.style.height = window.innerHeight - 70 + "px";
     pane.$data.paneStyle.width = window.innerWidth - 260 + "px";
     pane.$data.paneStyle.height = window.innerHeight - 70 + "px";
     pane.$data.tableStyle.height =
       window.innerHeight - 70 - 80 - 58 - 20 + "px";
     darkpane1.$data.dkp1Style.height = window.innerHeight + "px";
-    darkpane1.$data.adpStyle = {
+    darkpane1.$data.windowStyle = {
+      left: (window.innerWidth - 350) / 2 + "px",
+      top: (window.innerHeight - 240) / 2 + "px"
+    };
+    darkpane2.$data.windowStyle = {
       left: (window.innerWidth - 350) / 2 + "px",
       top: (window.innerHeight - 240) / 2 + "px"
     };
